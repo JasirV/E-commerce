07f1cf8474ef09a9f06a95532eacafae3c463725
JasirV <jasirmon133@gmail.com>
2024-03-06 21:21:49 +0530

    dog

--- Client/src/Componets/Dog.js
+++ Client/src/Componets/Dog.js
@@ -1,24 +1,53 @@
-import React, { useContext } from "react";
-import { AllContext } from "../App";
+import React, { useContext, useEffect, useState } from "react";
+import { AllContext, Axios } from "../App";
 import { Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { Footer } from "./Footer";
 import Navigation from "./Navigation";
+import {toast} from "react-toastify"
+import axios from "axios";
+const userId=localStorage.getItem('userId');
 
 const Dog = () => {
-  const { product } = useContext(AllContext);
   const Navigate = useNavigate();
-  const dogProduct = product.filter((iteam) => iteam.Animal === "Dog");
+  const [product,setProduct]=useState([])
+  const category="dog"
+useEffect(()=>{
+  const DogProduct =async ()=>{
+    try {
+      const response=await axios.get(`http://localhost:3001/users/products/category/${category}`);
+      setProduct(response.data.product)
+      console.log(response.data.product);
+    } catch (error) {
+      console.log("Error Fetching The Product",error);
+      toast.error('Error');
+    }
+  }
+  DogProduct();
+},[])
+
+const addToWishList=async(id)=>{
+  try {
+    const response =await Axios.post(`users/${userId}/wishlist`,{productId:id});
+    if(response.status===200){
+      return toast.success('Prodct Added To The Whislist!')
+    }
+  } catch (error) {
+    console.error('Error Adding To The Whislist',error);
+    toast.error(error.response.data.message)
+  }
+}
+
   return (
     <div>
       <Navigation />
       <div className="d-flex flex-wrap m-3 justify-content-center">
-        {dogProduct.map((item, index) => (
+        {product.map((item, index) => (
           <Card
             onClick={() => {
-              Navigate(`/View/${item.Id}`);
+              Navigate(`/View/${item._id}`);
             }}
-            key={item.id || index}
+            key={item._id || index}
             className="m-2"
             style={{ width: "16rem", overflow: "hidden", margin: "auto" }}>
             <div
@@ -34,14 +63,13 @@ const Dog = () => {
               <Card.Img
                 className="img-fluid"
                 variant="top"
-                src={item.Image}
+                src={item.image}
                 style={{ height: "16rem", width: "12rem" }}
               />
             </div>
             <Card.Body>
-              <h6 className="mt-1">₹{item.Price}</h6>
-              <del className="text-secondary">₹{item.OldPrice}</del>
-              <Card.Title>{item.ProductName}</Card.Title>
+              <h6 className="mt-1">₹{item.price}</h6>
+              <Card.Title>{item.title}</Card.Title>
             </Card.Body>
           </Card>
         ))}
